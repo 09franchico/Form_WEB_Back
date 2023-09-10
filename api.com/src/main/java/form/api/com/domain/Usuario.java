f7ca598cf3b2eeefd08b4658abec8d2e3b3d194a
Francisco <francisco.santossouzafilho@gmail.com>
2023-09-10 17:21:54 -0400

    Implemeteção do JWT

--- api.com/src/main/java/form/api/com/domain/Usuario.java
+++ api.com/src/main/java/form/api/com/domain/Usuario.java
@@ -1,12 +1,8 @@
 package form.api.com.domain;
 import jakarta.persistence.*;
-import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
